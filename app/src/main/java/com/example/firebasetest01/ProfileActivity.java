831b7e745bb16d6497f6b3f7a7512d8f5670b1f3
drather <drather@naver.com>
2019-11-14 21:56:39 +0900

    activity_profile에 회원정보수정, 정보저장 버튼 추가 ProfileActivity에 회원정보수정하는 부분 구현

--- app/src/main/java/com/example/firebasetest01/ProfileActivity.java
+++ app/src/main/java/com/example/firebasetest01/ProfileActivity.java
@@ -2,6 +2,9 @@ package com.example.firebasetest01;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.text.Editable;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -26,22 +29,25 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
+    TextView textView_email_profile, textView_name_profile;
+    EditText editText_phoneNum_profile, editText_carNum_profile, editText_carType_profile;
+    Intent intent;
+    Button btn_modifyProfile, btn_confirm;
+    String ID = "";
+
     @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_profile);
 
-            Intent intent = getIntent();
-            String ID = intent.getStringExtra("userEmail");
+            intent = getIntent();
+            ID = intent.getStringExtra("userEmail");
 
             databaseReference.child("User List").child(ID).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     //이 밑에 5줄은 getValue()시험
                     UserData userDataFromDB = dataSnapshot.getValue(UserData.class);
-
-                    TextView textView_email_profile, textView_name_profile;
-                    EditText editText_phoneNum_profile, editText_carNum_profile, editText_carType_profile;
 
                     textView_email_profile = findViewById(R.id.textView_email_profile);
                     textView_name_profile = findViewById(R.id.textView_name_profile);
@@ -62,9 +68,37 @@ public class ProfileActivity extends AppCompatActivity {
                 }
             });
 
+        btn_modifyProfile = findViewById(R.id.btn_modifyProfile);
+        btn_confirm = findViewById(R.id.btn_confirm);
+        btn_modifyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText_phoneNum_profile.setEnabled(true);
+                editText_carNum_profile.setEnabled(true);
+                editText_carType_profile.setEnabled(true);
+                btn_confirm.setEnabled(true);
+            }
+        });
 
-        }
-    public void modifyProfile() {
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String modified_phoneNum = editText_phoneNum_profile.getText().toString();
+                String modified_carNum = editText_carNum_profile.getText().toString();
+                String modified_carType = editText_carType_profile.getText().toString();
+
+                databaseReference.child("User List").child(ID).child("phoneNum").setValue(modified_phoneNum);
+                databaseReference.child("User List").child(ID).child("carNum").setValue(modified_carNum);
+                databaseReference.child("User List").child(ID).child("carType").setValue(modified_carType);
+                Log.d("modification", "modified_phoneNum" + modified_phoneNum);
+
+                editText_phoneNum_profile.setEnabled(false);
+                editText_carNum_profile.setEnabled(false);
+                editText_carType_profile.setEnabled(false);
+
+                btn_confirm.setEnabled(false);
+            }
+        }) ;
 
     }
 }
